0e644ec9b0cf7d1dcec1dd2f9ae0b7c6a4569145
Bobby Rogers <rrogers82@gmail.com>
2014-02-23 17:01:13 -0500

    Removing webGL after merging in css changes

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -6,32 +6,5 @@ socket.on('orient', function(data){
 var gl;
 
 window.addEventListener('load', function(event){
-    var canvas = document.getElementById('gl');
-    gl = initWebGL(canvas);
 
-    if (gl) {
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);                      // Set clear color to black, fully opaque
-        gl.enable(gl.DEPTH_TEST);                               // Enable depth testing
-        gl.depthFunc(gl.LEQUAL);                                // Near things obscure far things
-        gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);      // Clear the color as well as the depth buffer.
-    }
 });
-
-
-function initWebGL(canvas) {
-    gl = null;
-
-    try {
-        // Try to grab the standard context. If it fails, fallback to experimental.
-        gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch(e) {}
-
-    // If we don't have a GL context, give up now
-    if (!gl) {
-        alert("Unable to initialize WebGL. Your browser may not support it.");
-        gl = null;
-    }
-
-    return gl;
-}
